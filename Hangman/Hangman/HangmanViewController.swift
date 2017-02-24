2e9caf4c49a19b0cb6009d1f87c1350fff422d91
Rexana Church <rexanachurch@Rexanas-MacBook.local>
2017-02-23 19:52:16 -0800

    displays incorrect letters correctly

--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -10,21 +10,73 @@ import UIKit
 
 class HangmanViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var blanksLabel: UILabel!
+    @IBOutlet weak var incorrectGuessesLabel: UILabel!
+    
+    var letter = ""
+    var phrase = ""
+    var phraseLength = 0
+    var displayString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let hangmanPhrases = HangmanPhrases()
         // Generate a random phrase for the user to guess
-        let phrase: String = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()
+        phraseLength = phrase.characters.count
         print(phrase)
+        
+        displayInitialDashes()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    func displayInitialDashes() {
+        for _ in 0..<phraseLength {
+            displayString += "-"
+        }
+        updateDisplayString()
+    }
+    
+    func updateDisplayString() {
+        blanksLabel.text = displayString
+    }
+    
+    func addCorrectLetter() {
+        let range: Range<String.Index> = phrase.range(of: letter)!
+        let indexOfLetter: Int = phrase.distance(from: phrase.startIndex, to: range.lowerBound)
 
+        var chars = Array(phrase.characters)  // gets an array of characters
+        chars[indexOfLetter] = Character(letter)
+        displayString = String(chars)
+
+    }
+    
+    func addIncorrectLetter() {
+        incorrectGuessesLabel.text? += " " + letter
+    }
+    
+    @IBAction func letterButtonWasPressed(_ sender: UIButton) {
+        if let label = sender.titleLabel {
+            if let text = label.text {
+                letter = text
+            }
+        }
+    }
+
+    @IBAction func guessButtonWasPressed(_ sender: UIButton) {
+        if phrase.range(of:letter) != nil{
+            // display on screen
+            addCorrectLetter()
+            updateDisplayString()
+        } else {
+            addIncorrectLetter()
+        }
+    }
     /*
     // MARK: - Navigation
 
